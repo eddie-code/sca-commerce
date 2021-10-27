250e6c90b3bb4e5ead95397321ee899569396719
eddie-code <231071372@qq.com>
2021-10-28 01:10:41 +0800

    5.3.1.2 方式二：自定义告警

---
+++ sca-commerce-admin/src/main/java/com/edcode/commerce/notifier/QinyiNotifier.java
@@ -0,0 +1,46 @@
+package com.edcode.commerce.notifier;
+
+import de.codecentric.boot.admin.server.domain.entities.Instance;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
+import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import de.codecentric.boot.admin.server.notify.AbstractEventNotifier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author eddie.lee
+ * @blog blog.eddilee.cn
+ * @description 自定义告警
+ */
+@Slf4j
+@Component
+@SuppressWarnings("all")
+public class QinyiNotifier extends AbstractEventNotifier {
+
+    protected QinyiNotifier(InstanceRepository repository) {
+        super(repository);
+    }
+
+    /**
+     * <h2>实现对事件的通知</h2>
+     * */
+    @Override
+    protected Mono<Void> doNotify(InstanceEvent event, Instance instance) {
+
+        return Mono.fromRunnable(() -> {
+
+            if (event instanceof InstanceStatusChangedEvent) {
+                log.info("Instance Status Change: [{}], [{}], [{}]",
+                        instance.getRegistration().getName(), event.getInstance(),
+                        ((InstanceStatusChangedEvent) event).getStatusInfo().getStatus());
+            } else {
+                log.info("Instance Info: [{}], [{}], [{}]",
+                        instance.getRegistration().getName(), event.getInstance(),
+                        event.getType());
+            }
+
+        });
+    }
+}
