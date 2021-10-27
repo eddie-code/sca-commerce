69014fb096f1dddfe813363715a821a07f4ff34c
eddie-code <231071372@qq.com>
2021-10-27 23:20:28 +0800

    5.1.3 应用注册到 SpringBoot Admin Server

--- sca-commerce-alibaba-nacos-client/src/main/java/com/edcode/commerce/NacosClientApplication.java
+++ sca-commerce-alibaba-nacos-client/src/main/java/com/edcode/commerce/NacosClientApplication.java
@@ -3,12 +3,14 @@ package com.edcode.commerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * @author eddie.lee
  * @blog blog.eddilee.cn
  * @description Nacos Client 工程启动入口
  */
+@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosClientApplication {
