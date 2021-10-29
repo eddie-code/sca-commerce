20adc6d559245533230022beb7e4c61082f47384
eddie-code <231071372@qq.com>
2021-10-29 20:57:35 +0800

    7.2.1 谓词Predicate的原理与应用

---
+++ sca-commerce-gateway/src/main/java/com/edcode/commerce/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.edcode.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author eddie.lee
+ * @blog blog.eddilee.cn
+ * @description 网关启动入口
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
