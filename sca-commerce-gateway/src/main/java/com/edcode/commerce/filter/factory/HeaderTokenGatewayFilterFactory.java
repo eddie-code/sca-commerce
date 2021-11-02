5a7d782f586aa2153f60bbb194603100a73bc860
eddie-code <231071372@qq.com>
2021-11-02 16:11:31 +0800

    7.13.1 代码与文件两种方式配置网关路由 （二选一）

--- sca-commerce-gateway/src/main/java/com/edcode/commerce/filter/factory/HeaderTokenGatewayFilterFactory.java
+++ sca-commerce-gateway/src/main/java/com/edcode/commerce/filter/factory/HeaderTokenGatewayFilterFactory.java
@@ -9,6 +9,14 @@ import org.springframework.stereotype.Component;
  * @author eddie.lee
  * @blog blog.eddilee.cn
  * @description 工厂模式
+ *
+ *     "filters": [
+ *       {
+ *         "name": "HeaderToken"
+ *       },
+ *       ...
+ *
+ *   "name": "HeaderToken" 其实就是 HeaderTokenGatewayFilterFactory 只是gateway 会过滤掉 GatewayFilterFactory 只取 HeaderToken
  */
 @Component
 public class HeaderTokenGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
