ada0ab42b9535f58a44af7ed530ca5e649b639dc
eddie-code <231071372@qq.com>
2021-11-15 17:47:42 +0800

    10.5.1 商品信息对象定义及转换方法

--- sca-commerce-service/sca-commerce-goods-service/src/main/java/com/edcode/commerce/entity/ScaCommerceGoods.java
+++ sca-commerce-service/sca-commerce-goods-service/src/main/java/com/edcode/commerce/entity/ScaCommerceGoods.java
@@ -3,9 +3,15 @@ package com.edcode.commerce.entity;
 import java.util.Date;
 import javax.persistence.*;
 
+import com.alibaba.fastjson.JSON;
+import com.edcode.commerce.constant.BrandCategory;
+import com.edcode.commerce.constant.GoodsCategory;
+import com.edcode.commerce.constant.GoodsStatus;
 import com.edcode.commerce.converter.BrandCategoryConverter;
 import com.edcode.commerce.converter.GoodsCategoryConverter;
 import com.edcode.commerce.converter.GoodsStatusConverter;
+import com.edcode.commerce.goods.GoodsInfo;
+import com.edcode.commerce.goods.SimpleGoodsInfo;
 import org.hibernate.annotations.GenericGenerator;
 import java.io.Serializable;
 
@@ -43,7 +49,7 @@ public class ScaCommerceGoods implements Serializable {
 	@Column(name = "goods_category", nullable = false)
 	// JPA有的，其实就是自动拆装箱的操作，可以转换类型存入DB
 	@Convert(converter = GoodsCategoryConverter.class)
-	private String goodsCategory;
+	private GoodsCategory goodsCategory;
 
 	/**
 	 * 品牌分类
@@ -51,7 +57,7 @@ public class ScaCommerceGoods implements Serializable {
 	@Column(name = "brand_category", nullable = false)
 	// JPA有的，其实就是自动拆装箱的操作，可以转换类型存入DB
 	@Convert(converter = BrandCategoryConverter.class)
-	private String brandCategory;
+	private BrandCategory brandCategory;
 
 	/**
 	 * 商品名称
@@ -77,7 +83,7 @@ public class ScaCommerceGoods implements Serializable {
 	@Column(name = "goods_status", nullable = false)
 	// JPA有的，其实就是自动拆装箱的操作，可以转换类型存入DB
 	@Convert(converter = GoodsStatusConverter.class)
-	private Integer goodsStatus;
+	private GoodsStatus goodsStatus;
 
 	/**
 	 * 商品价格: 单位: 分、厘
@@ -117,4 +123,61 @@ public class ScaCommerceGoods implements Serializable {
 	@Column(name = "update_time", nullable = false)
 	private Date updateTime;
 
+	/**
+	 * <h2>将 GoodsInfo 转成实体对象</h2>
+	 * */
+	public static ScaCommerceGoods to(GoodsInfo goodsInfo) {
+		ScaCommerceGoods scaCommerceGoods = new ScaCommerceGoods();
+		scaCommerceGoods.setGoodsCategory(GoodsCategory.of(goodsInfo.getGoodsCategory()));
+		scaCommerceGoods.setBrandCategory(BrandCategory.of(goodsInfo.getBrandCategory()));
+		scaCommerceGoods.setGoodsName(goodsInfo.getGoodsName());
+		scaCommerceGoods.setGoodsPic(goodsInfo.getGoodsPic());
+		scaCommerceGoods.setGoodsDescription(goodsInfo.getGoodsDescription());
+		// TODO 可以增加一个审核的过程: 后续有空在搞
+		scaCommerceGoods.setGoodsStatus(GoodsStatus.ONLINE);
+		scaCommerceGoods.setPrice(goodsInfo.getPrice());
+		scaCommerceGoods.setSupply(goodsInfo.getSupply());
+		scaCommerceGoods.setInventory(goodsInfo.getSupply());
+		scaCommerceGoods.setGoodsProperty(
+				JSON.toJSONString(goodsInfo.getGoodsProperty())
+		);
+
+		return scaCommerceGoods;
+	}
+
+	/**
+	 * <h2>将实体对象转成 GoodsInfo 对象</h2>
+	 * */
+	public GoodsInfo toGoodsInfo() {
+		GoodsInfo goodsInfo = new GoodsInfo();
+		goodsInfo.setId(this.id);
+		goodsInfo.setGoodsCategory(this.goodsCategory.getCode());
+		goodsInfo.setBrandCategory(this.brandCategory.getCode());
+		goodsInfo.setGoodsName(this.goodsName);
+		goodsInfo.setGoodsPic(this.goodsPic);
+		goodsInfo.setGoodsDescription(this.goodsDescription);
+		goodsInfo.setGoodsStatus(this.goodsStatus.getStatus());
+		goodsInfo.setPrice(this.price);
+		goodsInfo.setGoodsProperty(
+				JSON.parseObject(this.goodsProperty, GoodsInfo.GoodsProperty.class)
+		);
+		goodsInfo.setSupply(this.supply);
+		goodsInfo.setInventory(this.inventory);
+		goodsInfo.setCreateTime(this.createTime);
+		goodsInfo.setUpdateTime(this.updateTime);
+
+		return goodsInfo;
+	}
+
+	/**
+	 * <h2>将实体对象转成 SimpleGoodsInfo 对象</h2>
+	 * */
+	public SimpleGoodsInfo toSimple() {
+		SimpleGoodsInfo goodsInfo = new SimpleGoodsInfo();
+		goodsInfo.setId(this.id);
+		goodsInfo.setGoodsName(this.goodsName);
+		goodsInfo.setGoodsPic(this.goodsPic);
+		goodsInfo.setPrice(this.price);
+		return goodsInfo;
+	}
 }
