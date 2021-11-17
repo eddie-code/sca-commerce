88b7c6f7c8fe5fa6a2f4c3d0c510629912d889e0
eddie-code <231071372@qq.com>
2021-11-17 22:17:36 +0800

    11.2.1.2 RestTemplate 方式模拟获取 Token

---
+++ sca-commerce-alibaba-nacos-client/src/main/java/com/edcode/commerce/controller/CommunicationController.java
@@ -0,0 +1,34 @@
+package com.edcode.commerce.controller;
+
+import com.edcode.commerce.service.communication.UseRestTemplateService;
+import com.edcode.commerce.vo.JwtToken;
+import com.edcode.commerce.vo.UsernameAndPassword;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author eddie.lee
+ * @blog blog.eddilee.cn
+ * @description 微服务通信 Controller
+ */
+@RestController
+@RequestMapping("/communication")
+@RequiredArgsConstructor
+public class CommunicationController {
+
+	private final UseRestTemplateService restTemplateService;
+
+	@PostMapping("/rest-template")
+	public JwtToken getTokenFromAuthorityService(@RequestBody UsernameAndPassword usernameAndPassword) {
+		return restTemplateService.getTokenFromAuthorityService(usernameAndPassword);
+	}
+
+	@PostMapping("/rest-template-load-balancer")
+	public JwtToken getTokenFromAuthorityServiceWithLoadBalancer(@RequestBody UsernameAndPassword usernameAndPassword) {
+		return restTemplateService.getTokenFromAuthorityServiceWithLoadBalancer(usernameAndPassword);
+	}
+
+}
---
+++ sca-commerce-alibaba-nacos-client/src/main/java/com/edcode/commerce/service/communication/UseRestTemplateService.java
@@ -0,0 +1,84 @@
+package com.edcode.commerce.service.communication;
+
+import com.alibaba.fastjson.JSON;
+import com.edcode.commerce.constant.CommonConstant;
+import com.edcode.commerce.vo.JwtToken;
+import com.edcode.commerce.vo.UsernameAndPassword;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author eddie.lee
+ * @blog blog.eddilee.cn
+ * @description 使用 RestTemplate 实现微服务通信
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UseRestTemplateService {
+
+	/**
+	 * 随机挑选客户端（负载均衡）
+	 */
+	private final LoadBalancerClient loadBalancerClient;
+
+	/**
+	 * 从授权服务中获取 JwtToken
+	 */
+	public JwtToken getTokenFromAuthorityService(UsernameAndPassword usernameAndPassword) {
+
+		// 第一种方式: 写死 url
+		String requestUrl = "http://127.0.0.1:7000/scacommerce-authority-center" + "/authority/token";
+		log.info("RestTemplate请求url和正文: [{}], [{}]",
+				requestUrl,
+				JSON.toJSONString(usernameAndPassword)
+		);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		return new RestTemplate().postForObject(
+				requestUrl,
+				new HttpEntity<>(JSON.toJSONString(usernameAndPassword), headers),
+				JwtToken.class
+		);
+	}
+
+	/**
+	 * 从授权服务中获取 JwtToken, 且带有负载均衡
+	 */
+	public JwtToken getTokenFromAuthorityServiceWithLoadBalancer(UsernameAndPassword usernameAndPassword) {
+
+		// 第二种方式: 通过注册中心拿到服务的信息(是所有的实例), 再去发起调用
+		ServiceInstance serviceInstance = loadBalancerClient.choose(CommonConstant.AUTHORITY_CENTER_SERVICE_ID);
+		log.info("Nacos客户端信息: [{}], [{}], [{}]",
+				serviceInstance.getServiceId(),
+				serviceInstance.getInstanceId(),
+				JSON.toJSONString(serviceInstance.getMetadata())
+		);
+
+		// 与第一种方式区别，就是不用在写死 url, 从 loadBalancerClient 里面获取
+		String requestUrl = String.format(
+				"http://%s:%s/scacommerce-authority-center/authority/token",
+				serviceInstance.getHost(),
+				serviceInstance.getPort()
+		);
+		log.info("登录请求url和正文: [{}], [{}]", requestUrl, JSON.toJSONString(usernameAndPassword));
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		return new RestTemplate().postForObject(
+				requestUrl,
+				new HttpEntity<>(JSON.toJSONString(usernameAndPassword), headers),
+				JwtToken.class
+		);
+	}
+}
